a916b3604b0e582d324b24455dd7799a3e72ed7d
jmguerro <jmguerro@mercadona.es>
2023-03-31 15:51:17 +0200

    obstacle class

---
+++ src/main/java/assets/cardinal/Obstacle.java
@@ -0,0 +1,15 @@
+package assets.cardinal;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Obstacle {
+
+ private int obstacleX;
+ private int obstacleY;
+
+}
